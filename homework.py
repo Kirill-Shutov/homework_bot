51ba02e0eaaf59e1bc847c8fad2e0ac16e96c3f6
Кирилл Шутов <shutovka14@yandex.ru>
2022-07-12 17:58:48 +0300

    first step

--- homework.py
+++ homework.py
@@ -1,11 +1,21 @@
-...
+import logging
+import os
+import sys
+
+import requests
+
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import CommandHandler, Updater
+
+from dotenv import load_dotenv
 
 load_dotenv()
+"""Спрятал токены в .env и получаю его от туда с помощью load_dotenv()
+и передаю в веременные"""
 
-
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -18,66 +28,83 @@ HOMEWORK_STATUSES = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+"""Задана глобальная конфигурация для всех логгеров"""
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
+    filename='main.log',
+    filemode='w'
+    )
+
+logger = logging.getLogger(__name__)  
+handler = logging.StreamHandler(sys.stdout)
+logger.addHandler(handler)
+
 
 def send_message(bot, message):
-    ...
+    """Отправляет сообщения."""
+    try:
+        bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+        logger.info(f'Сообщение в чат отправлено: {message}')
+    except Exception as error:
+        logger.error(f'Сбой при отправке сообщения в чат: {error}')
+
+# def get_api_answer(current_timestamp):
+#     timestamp = current_timestamp or int(time.time())
+#     params = {'from_date': timestamp}
+
+#     ...
 
 
-def get_api_answer(current_timestamp):
-    timestamp = current_timestamp or int(time.time())
-    params = {'from_date': timestamp}
+# def check_response(response):
 
-    ...
+#     ...
 
 
-def check_response(response):
+# def parse_status(homework):
+#     homework_name = ...
+#     homework_status = ...
 
-    ...
+#     ...
 
+#     verdict = ...
 
-def parse_status(homework):
-    homework_name = ...
-    homework_status = ...
+#     ...
 
-    ...
-
-    verdict = ...
-
-    ...
-
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+#     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
-def check_tokens():
-    ...
+# def check_tokens():
+#     """Проверяет, передались ли все токены корректно"""
+#     return all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID])
 
 
-def main():
-    """Основная логика работы бота."""
+# def main():
+#     """Основная логика работы бота."""
 
-    ...
+#     ...
 
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())
+#     bot = telegram.Bot(token=TELEGRAM_TOKEN)
+#     current_timestamp = int(time.time())
 
-    ...
+#     ...
 
-    while True:
-        try:
-            response = ...
+#     while True:
+#         try:
+#             response = ...
 
-            ...
+#             ...
 
-            current_timestamp = ...
-            time.sleep(RETRY_TIME)
+#             current_timestamp = ...
+#             time.sleep(RETRY_TIME)
 
-        except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            ...
-            time.sleep(RETRY_TIME)
-        else:
-            ...
+#         except Exception as error:
+#             message = f'Сбой в работе программы: {error}'
+#             ...
+#             time.sleep(RETRY_TIME)
+#         else:
+#             ...
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
